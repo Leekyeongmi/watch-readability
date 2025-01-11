3321356fc6455669917ca8ec9146adb10ae88ace
Lisa <aoo167.10@gmail.com>
2025-01-11 12:16:24 +0900

    anime-f3

--- src/components/MovingClock.jsx
+++ src/components/MovingClock.jsx
@@ -16,8 +16,8 @@ export default function MovingClock({ type = '1' }) {
 
   // Ease-in-out 함수
   const easeInOut = (progress) => {
-    const easeInPower = 1;  // ease-in 강도
-    const easeOutPower = 1; // ease-out 강도
+    const easeInPower = 2;  // ease-in 강도
+    const easeOutPower = 2; // ease-out 강도
     if (progress < 0.5) {
       // Ease-in 구간
       return Math.pow(progress * 2, easeInPower) / 2;
