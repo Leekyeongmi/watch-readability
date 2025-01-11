d44ca40bc828af0a88300e7145df96a28b3ee1a0
kyeongmi <lee@kyeongmis-MacBook-Pro.local>
2025-01-11 23:37:39 +0900

    test

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -12,8 +12,8 @@ const GlobalStyle = createGlobalStyle`
   box-shadow: none !important;
   font-display: swap;
   -webkit-user-drag: none;
-  overscroll-behavior: none;
   box-sizing:border-box;
+  overscroll-behavior: contain;
   font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
 }
 
